239d59a64be6aa78c6089b623d0bfdaab5179695
Gerald Maguire <g.maguire85@btinternet.com>
2018-06-23 11:20:11 +0100

    adding fldr for jenkins scripts and a test jenkins script

---
+++ jenkins_scripts/jenkins_test.py
@@ -0,0 +1,17 @@
+import jenkins
+
+username=input("Please enter your username:")
+password=input("Please enter your password:")
+
+#server = jenkins.Jenkins('http://159.65.89.217:8080/', username='admin', password='admin123')
+server = jenkins.Jenkins('http://159.65.89.217:8080/', username, password)
+user = server.get_whoami()
+version = server.get_version()
+print('Hello %s from Jenkins %s' % (user['fullName'], version))
+
+print("Total Jenkins Jobs are:", server.jobs_count())
+
+job_name = server.get_all_jobs(2)
+print(job_name)
+
+server.build_job('Lab Pipeline')
