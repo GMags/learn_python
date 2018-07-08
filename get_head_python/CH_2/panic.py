77d50bcf5dc5b1af709cd69b1f1c7ad2440d9eb5
Gerald Maguire <g.maguire85@btinternet.com>
2018-07-08 12:18:39 +0100

    Adding the panic exercise

---
+++ get_head_python/CH_2/panic.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3.6
+
+phrase = "Don't panic!"
+plist = list(phrase)
+
+print(phrase)
+print(plist)
+
+plist.pop(0)
+plist.remove("'")
+
+for i in range(4):
+    plist.pop()
+
+plist.extend([plist.pop(), plist.pop()])
+plist.insert(2, plist.pop(3))
+
+new_phrase = ''.join(plist)
+print(plist)
+print(new_phrase)
