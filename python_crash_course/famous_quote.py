6caac7ff6b72a3a7cee581eab44039cffd5ab27d
Gerald Maguire <g.maguire85@btinternet.com>
2019-07-22 14:09:50 +0100

    stripping text exercises

---
+++ python_crash_course/famous_quote.py
@@ -0,0 +1,9 @@
+first_name = 'jurgen'
+last_name = 'kloop'
+
+famous_person = f"{first_name} {last_name}"
+msg = f"{famous_person.title()} said: Lets talk about six baby!"
+
+print(msg)
+
+
---
+++ python_crash_course/stripping_names.py
@@ -0,0 +1,8 @@
+our_captain = "\t\tSteven\n\t\tGerrard\t\t"
+print(our_captain)
+
+print(our_captain.lstrip())
+
+print(our_captain.rstrip())
+
+print(our_captain.strip())
