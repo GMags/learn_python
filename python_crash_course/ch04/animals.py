09048ef3a701e507ef580206733b35bcfb8fe9aa
Gerald Maguire <g.maguire85@btinternet.com>
2020-02-13 23:34:31 +0000

    adding chapter 4 exercises

---
+++ python_crash_course/ch04/animals.py
@@ -0,0 +1,11 @@
+animals = ['dog', 'cat', 'horse', 'cow', 'sheep']
+
+for animal in animals:
+    print(animal)
+
+print(f"\n")
+
+for animal in animals:
+    print(f"A {animal} has four legs\n")
+
+print(f"You can eat all these animals, except a {animals[1]}")
---
+++ python_crash_course/ch04/pizzas.py
@@ -0,0 +1,11 @@
+pizzas = ['magherita', 'pepperoni', 'pineapple and ham', 'bbq chicken']
+
+for pizza in pizzas:
+    print(pizza)
+
+print(f"\n")
+
+for pizza in pizzas:
+    print(f"I like {pizza} pizza\n")
+
+print(f"I really love pizza!")
