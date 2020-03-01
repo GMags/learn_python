a3a30f56ad7146d637b28165292fcb2b39f65266
Gerald Maguire <g.maguire85@btinternet.com>
2020-03-01 18:17:23 +0000

    Updating pizza script

--- python_crash_course/ch04/pizzas.py
+++ python_crash_course/ch04/pizzas.py
@@ -1,4 +1,4 @@
-pizzas = ['magherita', 'pepperoni', 'pineapple and ham', 'bbq chicken']
+pizzas = ['magherita', 'pepperoni', 'pineapple and ham', 'BBQ chicken']
 
 for pizza in pizzas:
     print(pizza)
@@ -9,3 +9,37 @@ for pizza in pizzas:
     print(f"I like {pizza} pizza\n")
 
 print(f"I really love pizza!")
+
+new_pizzas = pizzas[:]
+new_pizzas.insert(0, 'sizzler')
+
+print(f"\n")
+
+print(f"The first 3 new pizzas are:")
+for pizza in new_pizzas[:3]:
+    print(pizza.title())
+
+print(f"\n")
+
+print(f"The items in the middle of the list are:")
+for pizza in new_pizzas[2:4]:
+    print(pizza.title())
+
+print(f"\n")
+
+print(f"The last 3 items of the list are:")
+for pizza in new_pizzas[2:]:
+    print(pizza.title())
+
+pizzas.append('Tuna and sweetcorn')
+
+print(f"\n")
+
+print(f"My favorite pizzas are:")
+for mypizza in pizzas:
+    print(mypizza.title())
+
+print(f"\nMy friend’s favorite pizzas are:")
+for friends_pizza in new_pizzas:
+    print(friends_pizza.title())
+
