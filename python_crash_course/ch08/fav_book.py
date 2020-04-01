d2c493be8d05766c44b31590e64208f9e8e1553c
Gerald Maguire <g.maguire85@btinternet.com>
2020-04-02 00:00:54 +0100

    Started chapter 8 exercises

---
+++ python_crash_course/ch08/fav_book.py
@@ -0,0 +1,5 @@
+def favourite_book(title):
+    """Create a function to take a parameter / argument called title"""
+    print(f"One of my favourite books is called: {title.title()}")
+
+favourite_book('python crash course')
---
+++ python_crash_course/ch08/message.py
@@ -0,0 +1,5 @@
+def display_messgae():
+    """Display a message about what i am learning"""
+    print("I am learning about functions in chapter 8 of python crash course")
+
+display_messgae()
