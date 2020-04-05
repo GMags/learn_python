3e75aa1e3f8c67f675c555d5bc3ab9294a433220
Gerald Maguire <g.maguire85@btinternet.com>
2020-04-05 13:03:14 +0100

    Adding chaper 9 exercises

--- python_crash_course/ch09/login_attempts.py
+++ python_crash_course/ch09/login_attempts.py
@@ -9,6 +9,7 @@ class User:
         self.date_of_birth = date_of_birth
         self.email = email
         self.location = location.title()
+        self.login_attempts = 0
 
     def describe_user(self):
         """Prints the user's profile information"""
@@ -22,6 +23,14 @@ class User:
         """Provide a greeting to the user"""
         print(f"\nWelcome {self.first_name} {self.last_name} to the site")
 
+    def increment_login_attempts(self):
+        """Increment the current login attempts by 1"""
+        self.login_attempts += 1
+
+    def reset_login_attempts(self):
+        """Resets the users login attempts to 0"""
+        self.login_attempts = 0
+
 
 ellie = User('ellie', 'maguire', '14', '07/07/2005', 'ellie.maguire@testdomain.com', 'cookstown')
 ellie.describe_user()
@@ -32,3 +41,17 @@ print("\n")
 nathan = User('nathan', 'colhoun', '34', '01/011984', 'nathan@natedogg.com', 'carrick')
 nathan.describe_user()
 nathan.greet_user()
+
+
+nathan.increment_login_attempts()
+nathan.increment_login_attempts()
+nathan.increment_login_attempts()
+nathan.increment_login_attempts()
+nathan.increment_login_attempts()
+login_attempts = f"The number of {nathan.first_name} {nathan.last_name} " \
+    f"login attempts is currently: {nathan.login_attempts}"
+print(login_attempts)
+nathan.reset_login_attempts()
+login_attempts = f"The number of {nathan.first_name} {nathan.last_name} " \
+    f"login attempts is currently: {nathan.login_attempts}"
+print(login_attempts)
