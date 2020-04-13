1f671a6ff80f516b8c554de1d3e90b065176e190
Gerald Maguire <g.maguire85@btinternet.com>
2020-04-13 21:58:30 +0100

    Adding a settings class and ship class for drawing on screen

--- python_crash_course/alien_invasion/alien_invasion.py
+++ python_crash_course/alien_invasion/alien_invasion.py
@@ -1,5 +1,7 @@
 import sys
 import pygame
+from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     """
@@ -9,9 +11,12 @@ class AlienInvasion:
     def __init__(self):
         """Init the game and create game resources"""
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_width))
         pygame.display.set_caption("Alien Invasion")
+
+        self.ship = Ship(self)
 
     def run_game(self):
         """Start main loop for the game"""
@@ -20,6 +25,9 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+
+            self.screen.fill(self.settings.bg_color) # white bg
+            self.ship.blitme()
 
             # Make the most recently drawn screen visible
             pygame.display.flip()
---
+++ python_crash_course/alien_invasion/settings.py
@@ -0,0 +1,9 @@
+class Settings:
+    """A class to store all of the settings for Alien Invasion"""
+
+    def __init__(self):
+        """Init the games settings"""
+        # Screen Settings
+        self.screen_width = 700
+        self.screen_height = 1200
+        self.bg_color = (230, 230, 230)
